db84e8167e3f8bb09e4aecdb34988ffa3fbfde16
Matthew Mattox <mmattox@support.tools>
2021-04-29 23:31:55 -0500

    testing

--- Login/run.sh
+++ Login/run.sh
@@ -23,37 +23,22 @@ EOF
 
 
 task(){
-rm -f cookie-"$1".txt
-echo "Registering user..."
-curl -k -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--X POST --data "$(register_post_data $1)" "${URL}/api/register"
-echo ""
-echo "Logging in..."
-curl -k -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--b cookie-"$1".txt -c cookie-"$1".txt \
--X POST --data "$(login_post_data $1)" "${URL}/api/login"
-echo ""
-echo "Getting user data..."
-curl -k -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--b cookie-"$1".txt -c cookie-"$1".txt \
--X GET "${URL}/api/user"
-echo ""
-echo "Logout..."
-curl -k -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--b cookie-"$1".txt -c cookie-"$1".txt \
--X POST "${URL}/api/logout"
-echo ""
-echo "Cleaning up cookie..."
-rm -rf cookie-"$1".txt
-echo ""
+  echo "Creating account for $1"
+  rm -f cookie-"$1".txt
+  echo "Registering user..."
+  curl -s -k -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "$(register_post_data $1)" "${URL}/api/register"
+  echo ""
+  echo "Logging in..."
+  curl -s -k -i -H "Accept: application/json" -H "Content-Type:application/json" -b cookie-"$1".txt -c cookie-"$1".txt -X POST --data "$(login_post_data $1)" "${URL}/api/login"
+  echo ""
+  echo "Getting user data..."
+  curl -s -k -i -H "Accept: application/json" -H "Content-Type:application/json" -b cookie-"$1".txt -c cookie-"$1".txt -X GET "${URL}/api/user"
+  echo ""
+  echo "Logout..."
+  curl -s -k -i -H "Accept: application/json" -H "Content-Type:application/json" -b cookie-"$1".txt -c cookie-"$1".txt -X POST "${URL}/api/logout"
+  echo ""
+  echo "Cleaning up cookie..."
+  rm -rf cookie-"$1".txt
 }
 
 while true;
